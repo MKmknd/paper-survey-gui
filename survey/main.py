0a8e99466decb37a2a3157827b46b91a6f420134
MKmknd <m-kondo@se.is.kit.ac.jp>
2020-05-16 15:57:13 +0900

    [add] comments

--- survey/main.py
+++ survey/main.py
@@ -16,17 +16,28 @@ class MyButton(Button):
         print(data)
 
 class RV(RecycleView):
+    """
+    Show the list of papers with RecycleView
+    """
     def __init__(self, **kwargs):
         super(RV, self).__init__(**kwargs)
-        self.data = [{'text': x} for x in sorted(glob.glob("./papers/*.pdf"))]
+        self.data = [{'text': x} for x in sorted(glob.glob("./ICSE*/*.pdf"))]
 
 
 class PaperListForm(BoxLayout):
+    """
+    Show the paper list with RV.
+    """
     def __init__(self, **kwargs):
         super(PaperListForm, self).__init__(**kwargs)
 
 
 class PaperInfo(BoxLayout):
+    """
+    To fill out the paper information
+    with this page.
+    """
+    # The property is used to move the info.
     result_list = ListProperty(['Unknown', 'None', 'Unknown', 'None'])
 
     def __init__(self, **kwargs):
@@ -34,11 +45,17 @@ class PaperInfo(BoxLayout):
         self.f_name = "temp"
 
     def load_data(self, f_name):
+        """
+        If the data is already filled out,
+        read the database and display the current info.
+        """
         self.result_list = db.load_curr_info(db_path, f_name)
         self.f_name = f_name
-        #self.result_list = ['Unknown', '{0}'.format(random.randint(1, 100)), 'Unknown', 'None']
 
     def enterInfo(self):
+        """
+        Enter the info into the database
+        """
         self.result_list = [self.ids['Q1'].text, self.ids['Q2'].text, self.ids['Q3'].text, self.ids['Q4'].text]
         db.update_info(db_path, self.f_name,
                         self.ids['Q1'].text,
@@ -48,11 +65,17 @@ class PaperInfo(BoxLayout):
 
 
 class PaperSurveyRoot(BoxLayout):
+    """
+    The root GUI
+    """
     def __init__(self, **kwargs):
         super(PaperSurveyRoot, self).__init__(**kwargs)
 
 
 class PaperSurveyApp(App):
+    """
+    Open the main application
+    """
 
     def __init__(self, **kwargs):
         super(PaperSurveyApp, self).__init__(**kwargs)
@@ -62,6 +85,9 @@ class PaperSurveyApp(App):
 
 
 def init_db():
+    """
+    Initialize the database
+    """
 
     conn = sqlite3.connect(db_path)
     cur = conn.cursor()
@@ -69,6 +95,7 @@ def init_db():
     cur.execute("CREATE TABLE IF NOT EXISTS papers(name TEXT PRIMARY KEY, Q1 TEXT, Q2 TEXT, Q3 TEXT, Q4 TEXT);")
     conn.commit()
     conn.close()
+
 if __name__=="__main__":
 
     init_db()
